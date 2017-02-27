85730265a1b66b205002a4125aab0ce2de9d3c20
Boyan <joshua.boyan@ad.pcc.edu>
2017-02-27 14:52:46 -0800

    Nested for in loops to access each cell on each sheet of workbook

--- toTable.js
+++ toTable.js
@@ -4,27 +4,41 @@ var process = require('process');
 var workingDirectory = process.cwd().slice(2);
 var XLSX = require('xlsx');
 var workbook = XLSX.readFile('resolutions.xlsx');
+var sheets = workbook.Sheets;
 var htmlFile = '';
-if (typeof process.argv[2] === 'undefined') {
-	console.log('\n' + 'Error:' +'\n' + 'You must enter the excel file you wish to build tables from as an argument' + '\n' + 'i.e., node toTable.js resolutions.xlsx');
+
+// Check that the file is the correct type
+if (process.argv[2].slice(-4) !== 'xlsx') {
+	console.log('\n' + 'This program will only convert xlsx files' + '\n' + 'Please enter correct file type');
+	return;
 } else {
-	var fileName = process.argv[2];
-	var newFileName = fileName.slice(0, -4) + 'html';
-}
-
-console.log(newFileName);
-// console.log(workbook.Sheets.Sheet1.A1);
-
-// iterate through each worksheet in the workbook
-/*for (var sheet in workbook.Sheets) {
-	if (typeof sheet !== 'undefined') {
-	console.log(sheet);
-		htmlFile += '<table summary="Board Meeting resolutions 2013-2014" class="turntable"><thead><tr>'
-		for(var cell in sheet) {
-			if (typeof cell !== 'undefined') {
-				
-			}
-		}
+	// Check to make sure user provides argument for command line
+	if (typeof process.argv[2] === 'undefined') {
+		console.log('\n' + 'Error:' + '\n' + 'You must enter the excel file you wish to build tables from as an argument' + '\n' + 'i.e., node toTable.js resolutions.xlsx');
+		return;
+	} else {
+		// Create the HTML file name to write the table to
+		var fileName = process.argv[2];
+		var newFileName = fileName.slice(0, -4) + 'html';
 	}
 }
-fs.writeFile('')*/
+//console.log(workbook.Sheets.Sheet1.A1);
+
+// Iterate through each worksheet in the workbook
+for (var sheet in sheets) {
+	if (typeof sheet !== 'undefined') {
+		htmlFile += '<table summary="" class="turntable">' + '\n' + '<thead>' + '\n' + '<tr>';
+		// Initialize counter to track cell rows
+		var counter = 0;
+		// Iterate over each cell value on the sheet
+		for (var cell in sheets[sheet]) {
+			console.log(sheets[sheet][cell].h);
+			counter++;
+		}
+		
+	}
+}
+/*fs.writeFile(newFileName, htmlFile, (err) => {
+	if (err) throw err;
+	console.log('It worked!');
+});*/
