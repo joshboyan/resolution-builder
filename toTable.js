504ffd64a0b9bd830e243e3a2fd988c7e3974fa9
Boyan <joshua.boyan@ad.pcc.edu>
2017-03-01 13:38:25 -0800

    Format table

--- toTable.js
+++ toTable.js
@@ -8,13 +8,14 @@ var sheets = workbook.Sheets;
 var htmlFile = '';
 
 // Check that the file is the correct type
-if (process.argv[2].slice(-4) !== 'xlsx') {
-	console.log('\n' + 'This program will only convert xlsx files' + '\n' + 'Please enter correct file type');
+
+// Check to make sure user provides argument for command line
+if (typeof process.argv[2] === 'undefined') {
+	console.log('\n' + 'Error:' + '\n' + 'You must enter the excel file you wish to build tables from as an argument' + '\n' + 'i.e., node toTable.js resolutions.xlsx');
 	return;
 } else {
-	// Check to make sure user provides argument for command line
-	if (typeof process.argv[2] === 'undefined') {
-		console.log('\n' + 'Error:' + '\n' + 'You must enter the excel file you wish to build tables from as an argument' + '\n' + 'i.e., node toTable.js resolutions.xlsx');
+	if (process.argv[2].slice(-4) !== 'xlsx') {
+		console.log('\n' + 'This program will only convert xlsx files' + '\n' + 'Please enter correct file type');
 		return;
 	} else {
 		// Create the HTML file name to write the table to
@@ -26,19 +27,38 @@ if (process.argv[2].slice(-4) !== 'xlsx') {
 
 // Iterate through each worksheet in the workbook
 for (var sheet in sheets) {
+	//console.log(Object.keys(sheets[sheet]));
 	if (typeof sheet !== 'undefined') {
-		htmlFile += '<table summary="" class="turntable">' + '\n' + '<thead>' + '\n' + '<tr>';
-		// Initialize counter to track cell rows
+		htmlFile += '<table summary="" class="turntable">' + '\n' + '<thead>';
 		var counter = 0;
 		// Iterate over each cell value on the sheet
 		for (var cell in sheets[sheet]) {
-			console.log(sheets[sheet][cell].h);
 			counter++;
+			// Protect against undefined values
+			if (typeof sheets[sheet][cell].w !== 'undefined') {
+				//The first row in the table
+				if (cell === 'A1') {
+					htmlFile += '\n' + '<tr>' + '\n' + '<th>' + sheets[sheet][cell].w.replace('&','&amp;').replace('-','&ndash;') + '</th>';
+				} else {
+					//The second row in the table closes the thead element
+					if (cell === 'A2') {
+						htmlFile += '\n' + '</tr>' + '\n' + '</thead>' + '\n' + '<tr>' + '\n' + '<th>' + '<a href ="' + 'link' + counter + '">' + sheets[sheet][cell].w.replace('&','&amp;').replace('-','&ndash;') + '</a>' + '</th>';
+					} else {
+						// The first cell in each row
+						if (cell.slice(0, 1) === 'A') {
+							htmlFile += '\n' + '</tr>' + '\n' + '<tr>' + '\n' + '<th>' + '<a href="' + 'link' + counter + '">' + sheets[sheet][cell].w.replace('&','&amp;').replace('-','&ndash;') + '</a>'+ '</th>';
+							//All the other cells
+						} else {
+							htmlFile += '\n' + '<td>' + sheets[sheet][cell].w.replace('&','&amp;').replace('-','&ndash;') + '</td>';
+						}
+					}
+				}
+			}
 		}
-		
+		htmlFile += '\n' +'</tr>' + '\n' +'</table>' + '\n';
 	}
 }
-/*fs.writeFile(newFileName, htmlFile, (err) => {
+fs.writeFile(newFileName, htmlFile, (err) => {
 	if (err) throw err;
 	console.log('It worked!');
-});*/
+});
